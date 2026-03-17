610fad907216837a2d83eac07da06fe814b058aa
Ben8547 <102626149+Ben8547@users.noreply.github.com>
2026-03-17 19:17:27 -0400

    Update STDP_Data_Wrangling.py

--- STDP_Data_Wrangling.py
+++ STDP_Data_Wrangling.py
@@ -8,13 +8,13 @@ from scipy.ndimage import minimum_filter1d, maximum_filter1d
 class STDP_Data_Processing:
     def __init__(self, excel_file:str, peak_voltage=None, reading_voltage=None, offsets=None):
         '''The assumption is that the reading pulses are in channel 1 with the pre-synaptic pulses and Ch2 contains only the post synaptic pulses'''
+        self.file_name = excel_file
         self.raw_data = pd.read_excel(excel_file,"Data")
         self.voltage1 = self.raw_data.VMeasCh1.to_numpy()
         self.voltage2 = self.raw_data.VMeasCh2.to_numpy()
         self.current1 = self.raw_data.IMeasCh1.to_numpy()
         self.current2 = self.raw_data.IMeasCh2.to_numpy()
         self.time_series = self.raw_data.TimeOutput.to_numpy()
-        self.FFT_voltage1, self.FFT_voltage2 = self.FFT_Voltage()
         self.dt = self.time_series[1] - self.time_series[0] # time between samples
 
         if peak_voltage == None:
@@ -85,6 +85,7 @@ class STDP_Data_Processing:
         plt.scatter(self.delta_T,self.weights)
         plt.ylabel("$\Delta w$%")
         plt.xlabel("$\Delta T$")
+        plt.title(self.file_name)
         plt.show()
 
     # Data Wrangling Functions:
@@ -148,12 +149,6 @@ class STDP_Data_Processing:
     def Determine_reading_voltage(self):
         return np.average(self.reading_wave[self.reading_wave>0.])
 
-    def FFT_Voltage(self):
-        FFTed_Voltage1 = fft.fft(self.voltage1)
-        FFTed_Voltage2 = fft.fft(self.voltage2)
-
-        return FFTed_Voltage1, FFTed_Voltage2
-
     def Compute_delta_T(self):
         delta_T = []
         start_count = 0
@@ -180,6 +175,41 @@ class STDP_Data_Processing:
         G_after = 1/R_after # conductivity after STDP pulse (current reading pulse)
         R_min = np.min(self.Resistances) # normalization factor
         return (G_after - G_before) * R_min * 100.
+    
+    # ------------------------
+    # Denoising functions
+    # ------------------------
+
+    def smooth_w_percent(self, method:str = 'Moving Average', window_size:int = 3):
+        if method == 'Moving Average':
+            new_weights = np.zeros_like(self.weights)
+            n = len(new_weights)
+            for i in range(n):
+                if i < window_size//2 and n-i < window_size//2: # ideally we always choose the window size so that this never happens - otherwise we get constant data
+                    new_weights[i] = np.average(self.weights)
+
+                elif i < window_size//2: # then there are not enough elements behind for a full window
+                    new_weights[i] = np.average(self.weights[:i+window_size//2])
+
+                elif n-i < window_size//2:
+                    new_weights[i] = np.average(self.weights[i-window_size//2:])
+
+                else:
+                    new_weights[i] = np.average(self.weights[i-window_size//2:i+window_size//2])
+
+        
+        self.weights = new_weights
+
+    def soft_smooth_voltages(self,threshold_freq:float):
+        for voltage in {"voltage1", "voltage2"}:
+            voltage_series = getattr(self, voltage)
+            Vfft = fft.fft(voltage_series)
+            V_freq = fft.fftfreq(voltage_series.size,self.dt)
+            Vfft[np.abs(V_freq) > threshold_freq] = 0.
+            setattr(self, voltage, fft.ifft(Vfft).real)
+
+
+
 
 
 class collated_data:
@@ -214,23 +244,11 @@ class collated_data:
 # tests
 if __name__ == "__main__":
     directory = "./Data/2026-03-11 STDP Testing/"
-    file_name = "F9_STDP.xls"
+    file_name = "F7_STDP.xls"
     file = directory + file_name
     Data = STDP_Data_Processing(file)
-    #print(Data.time_series)
-    #Data.view_voltages()
-    #print(Data.peak_voltage)
-    #Data.view_voltage2() # Ch2 is simply triangular pulses
-    #print(Data.FFT_voltage2)
-    #Data.view_current2()
-    #Data.view_current1()
-    #plt.plot(Data.time_series, Data.read_current1)
-    #plt.plot(Data.time_series, Data.read_current2)
-    #plt.show()
-    """plt.plot(Data.read_current1)
-    plt.plot(Data.read_current2)
-    plt.show()"""
-    print(Data.weights)
-    print(Data.delta_T)
-    Data.view_Weights()
-    #print(Data.delta_T)
+
+
+    Data.view_voltages()
+    Data.soft_smooth_voltages(800.)
+    Data.view_voltages()
